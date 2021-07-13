9e1d309ae431970ae6cd02f0b23cd6c608ff1a66
jabinjaleel <jabinjaleel04@gmail.com>
2021-07-13 23:07:01 +0530

    chatbot

--- lib/pages/chatBot.dart
+++ lib/pages/chatBot.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
+import 'package:mbls/widgets/textfield.dart';
 
 class DialogFlow extends StatelessWidget {
   // This widget is the root of your application.
@@ -26,27 +28,60 @@ class DialogFlowStateful extends StatefulWidget {
 }
 
 class DialogFlowState extends State<DialogFlowStateful> {
-  String l, s="light speed";
+  String l="";
+  String k="";
+  final query = TextEditingController();
+void initState()
+{
+  dialog("light speed");
+}
+  Future dialog(String q) async {
 
-  void initState() {
-    setState(() {
-      dialog(s).toString();
-    });
-  }
-
-  Future<String> dialog(String q) async {
     AuthGoogle authGoogle =
         await AuthGoogle(fileJson: "assets/dialogFlow.json").build();
     Dialogflow dialogflow =
         Dialogflow(authGoogle: authGoogle, language: Language.english);
-    AIResponse response =
-        await dialogflow.detectIntent(q);
-    l = response.getMessage();
-    return l;
+    AIResponse response = await dialogflow.detectIntent(q);
+    setState(() {
+      l = response.getMessage().toString();
+    });
+
+
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text(l));
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [Colors.blueAccent, Colors.lightBlueAccent])),
+          margin: EdgeInsets.only(
+            top: 100,
+            left: 10,
+          ),
+          child: Text(
+            l,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontFamily: "TimesNewRoman",
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        floatingActionButton: Row(children: [DisplayTextfield(
+          con: query,
+          i1: Icons.question_answer_outlined,
+          label: "Query",
+        ),IconButton(onPressed: (){setState(() {
+         dialog("What is programming language");
+        });}, icon: Icon(Icons.send))]));
   }
 }
