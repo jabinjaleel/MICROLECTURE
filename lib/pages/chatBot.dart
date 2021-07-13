5b6f7ba9570d855b8ebdfa99e22f484652dace8e
jabinjaleel <jabinjaleel04@gmail.com>
2021-07-13 20:59:45 +0530

    chatbot

--- lib/pages/chatBot.dart
+++ lib/pages/chatBot.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
 
-
 class DialogFlow extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -11,7 +9,6 @@ class DialogFlow extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: DialogFlowStateful(title: ''),
@@ -22,7 +19,6 @@ class DialogFlow extends StatelessWidget {
 class DialogFlowStateful extends StatefulWidget {
   DialogFlowStateful({Key key, this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -30,25 +26,27 @@ class DialogFlowStateful extends StatefulWidget {
 }
 
 class DialogFlowState extends State<DialogFlowStateful> {
-  void initState()
-  {
-    dialog();
-  }
- Future<void> dialog()
- async {
-   AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/dialogFlow.json").build();
-   Dialogflow dialogflow = Dialogflow(authGoogle: authGoogle,language: Language.english);
-   AIResponse response = await dialogflow.detectIntent("What is programming Language");
-   print(response.getMessage());
- }
+  String l, s="light speed";
 
+  void initState() {
+    setState(() {
+      dialog(s).toString();
+    });
+  }
+
+  Future<String> dialog(String q) async {
+    AuthGoogle authGoogle =
+        await AuthGoogle(fileJson: "assets/dialogFlow.json").build();
+    Dialogflow dialogflow =
+        Dialogflow(authGoogle: authGoogle, language: Language.english);
+    AIResponse response =
+        await dialogflow.detectIntent(q);
+    l = response.getMessage();
+    return l;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-        body:Text("hhh")
-
-    );
+    return Scaffold(body: Text(l));
   }
 }
